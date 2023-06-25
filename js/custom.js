ebf7679baaa0755ec391c6573af277c237ffa476
freeman <iviewn@gmail.com>
2023-06-25 11:43:58 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -89,7 +89,21 @@ $('.review__more').click(function() {
     $(this).siblings().addClass('full');
     $(this).hide();
 });
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        panel.classList.toggle("active");
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 86 + "px";
+        }
+    });
+}
 
 
 
