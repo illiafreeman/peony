0f39ae25cb9685880d2067c8d1b088544a3916e3
freeman <iviewn@gmail.com>
2023-06-26 06:02:21 +0300

    portfolio

--- js/custom.js
+++ js/custom.js
@@ -104,6 +104,15 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+$('.port__item').click(function() {
+    $('body').addClass('oh');
+    $('.modal-bg').addClass('vis');
+    $('#modal-port').addClass('vis');
 
-
+});
+$('.modal__close').click(function() {
+    $('body').removeClass('oh');
+    $('.modal-bg').removeClass('vis');
+    $('.modal-wrap').removeClass('vis');
+});
 
